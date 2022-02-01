b5a7ffd365c549f77fe773d75ee7d07c72dd20b5
Elwilliko <52544334+Elwilliko@users.noreply.github.com>
2022-02-01 11:38:43 -0500

    daots

--- evaWiliamChablaSRV/src/main/java/ec/edu/ups/pw59/evaWiliamChablaSRV/modelo/Vehiculo.java
+++ evaWiliamChablaSRV/src/main/java/ec/edu/ups/pw59/evaWiliamChablaSRV/modelo/Vehiculo.java
@@ -1,5 +1,22 @@
 package ec.edu.ups.pw59.evaWiliamChablaSRV.modelo;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+
 public class Vehiculo {
+	
+	@Id
+	@Column(name= "vei_placa")
+	private String placa;
+	
+	@Column(name= "vei_nombre")
+	private String nombre;
+	
+	@Column(name= "vei_modelo")
+	private String modelo;
+	
+	@Column(name= "vei_color")
+	private String color;
+	
 
 }
